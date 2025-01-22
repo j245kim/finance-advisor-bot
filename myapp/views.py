46d3a33adae56c2340a38d0f3a1eda6d7dcd49dd
Lee Seung Hun <zeromiles1123@gmail.com>
2025-01-22 09:42:48 +0900

    "Bllossom 정리"

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 from llama_cpp import Llama
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, PreTrainedTokenizer
 
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -11,6 +11,32 @@ from django.http import JsonResponse
 
 
 def bllossom(req, message):
+    def prompt_gen(tokenizer: PreTrainedTokenizer, messages: list[dict[str, str]], max_tokens) -> str:
+        """사전훈련 된 Tokenizer와 messages를 통해 prompt를 생성하는 함수
+        
+        Args:
+            tokenizer (PreTrainedTokenizer): 사전훈련 된 Tokenizer
+            messages (list[dict[str, str]]): 대화 내용이 담긴 messages
+            max_tokens (int): 생성할 prompt의 최대 길이
+            
+        Returns:
+            prompt (str): 생성된 prompt"""
+        
+        prompt = tokenizer.apply_chat_template(
+                                                    messages, 
+                                                    tokenize=False,
+                                                    add_generation_prompt=True
+                                                )
+
+        prompt = prompt.replace('<|begin_of_text|>', '').replace('<|eot_id|>', '')
+        prompt = prompt.replace('<|start_header_id|>', '\n\n<|start_header_id|>').strip()
+
+        if len(prompt) >= max_tokens:
+            del messages[1:3]
+            prompt = prompt_gen(tokenizer, messages, max_tokens)
+
+        return prompt
+    
     model_dir_path = rf'{Path(__file__).parents[1]}\models\Bllossom'
     model_path = rf'{model_dir_path}\llama-3.2-Korean-Bllossom-3B-gguf-Q4_K_M.gguf'
     tokenizer = AutoTokenizer.from_pretrained(model_dir_path)
@@ -24,15 +50,8 @@ def bllossom(req, message):
     messages.append({"role": "user", "content": message})
     
     # ChatBot 대답
-    max_tokens = model._n_ctx
-    prompt = tokenizer.apply_chat_template(
-                                                messages, 
-                                                tokenize=False,
-                                                add_generation_prompt=True
-                                            )
-
-    prompt = prompt.replace('<|begin_of_text|>', '').replace('<|eot_id|>', '')
-    prompt = prompt.replace('<|start_header_id|>', '\n\n<|start_header_id|>').strip()
+    max_tokens = 1024
+    prompt = prompt_gen(tokenizer, messages, max_tokens)
 
     generation_kwargs = {
                             "max_tokens": max_tokens,
@@ -41,20 +60,12 @@ def bllossom(req, message):
                             "top_p": 0.9,
                             "temperature": 0.6,
                         }
-    
-    if len(prompt) >= max_tokens:
-        completion = '죄송합니다. 최대 Context Window에 도달했습니다. 더 이상 답변이 불가능합니다.'
-    else:
-        try:
-            resonse_msg = model(prompt, **generation_kwargs)
-            completion = resonse_msg['choices'][0]['text'][len(prompt):].strip()
-        except Exception as e:
-            print(e)
-            completion = '죄송합니다. 대답을 생성하는 중에 오류가 발생했습니다.'
-        finally:
-            if model is not None:
-                model._sampler.close()
-                model.close()
+
+    resonse_msg = model(prompt, **generation_kwargs)
+    completion = resonse_msg['choices'][0]['text'][len(prompt):].strip()
+
+    model._sampler.close()
+    model.close()
 
     messages.append({"role": "assistant", "content": completion})
     with open(session_path, mode='w', encoding='utf-8', errors='ignore') as f:
